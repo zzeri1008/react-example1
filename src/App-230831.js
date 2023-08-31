b92da6171f7ce7557e608ff546bd6bd15c4f3536
hyunjung <hhhj.1486@gmail.com>
2023-08-31 21:41:24 +0900

    230831

--- src/App-230831.js
+++ src/App-230831.js
@@ -55,6 +55,31 @@ const Create = (props) => {
     </article>
 }
 
+/*
+update 기능을 구현해보자
+
+===>
+update 컴포넌트의 구조는 create와 비슷하지만 차이점이 있다.
+update를 할 때는 수정할 값의 초기 값을 읽어와야 하는데, 이 값(props)을 상태로 변환해주어야 한다.
+
+const [title, setTitle] = useState(props.title);
+const [body, setBody] = useState(props.body);
+
+props는 사용자가 컴포넌트로 전달하는 일종의 "명령"으로 우리가 직접 제어할 수 없다.
+따라서 이 값을 내부적으로 변경 가능한 상태로 변환해주어야 한다.
+상태는 컴포넌트 내부에서 값을 변경할 수 있는데, 이를 통해 우리가 수정할 값을 처리할 수 있다.
+
+
+값을 변경할 때 사용하는 이벤트는 onChange이다.
+일반 HTML에서 동작과 React에서의 동작에 차이점이 있다.
+HTML에서는 값이 바뀌고 마우스 포인트가 요소 밖으로 빠져나갈 때 호출되지만, React에서는 값이 변경될 때마다 호출된다.
+이 점을 이용하여 이벤트가 발생한 대상의 값을 상태로 갱신하여 화면에 보여지도록 할 수 있다.
+
+<p><input type="text" name="title" placeholder="title" value={title} onChange={event => {
+      setTitle(event.target.value);
+   }
+}/></p>
+*/
 const Update = (props) => {
     const [title, setTitle] = useState(props.title);
     const [body, setBody] = useState(props.body);
